6edc7a8ed927466dc51287f8b3905341976220a3
diawabdou <abdourahmane.diaw@gmail.com>
2026-05-07 15:00:38 -0400

    untrack test_solps_coupling pending validation

--- examples/test_solps_coupling/coupled_test/time_converter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/cloud/OpenEdge/examples/test_solps_coupling/coupled_test
-time python3 /home/cloud/OpenEdge/tools/converters/convert_solps_plasma.py solps/v0_1 --plasma-out /tmp/test_plasma.h5 --equ-file solps/baserun/g000001.00001_symm.X4.equ --b2fstate solps/v0_1/b2fstati
