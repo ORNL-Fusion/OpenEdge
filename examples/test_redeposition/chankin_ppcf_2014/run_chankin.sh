5c64e9ebfac22c0447f3703a67cd5256bd3cc935
abdoudiaw <abdourahmane.diaw@gmail.com>
2026-07-24 11:51:54 -0400

    chankin example: fig6 and tables 3-4 comparison

--- examples/test_redeposition/chankin_ppcf_2014/run_chankin.sh
+++ examples/test_redeposition/chankin_ppcf_2014/run_chankin.sh
@@ -31,4 +31,18 @@ for ne in $NE_LIST; do
     full|all)   run in.chankin_multi  $ne full   -var smode spatial -var dcad 10 ;;
   esac
 done
+
+# fig6: Chankin Fig. 6 / tables 3-4 conditions. Monoenergetic W0 at
+# E0 = R_E * (3/2)kTi (Te=Ti=100 eV), multi-ioniz + sheath E, p scanned via ne.
+if [[ $what == fig6 ]]; then
+  RE_TAGS=(01 03 1 3)          # R_E = 0.1 0.3 1 3
+  RE_EV=(15 45 150 450)        # E0 [eV]
+  NE6_LIST=(1e18 3e18 1e19 3e19 1e20 3e20 1e21)
+  for i in {1..4}; do
+    for ne in $NE6_LIST; do
+      run in.chankin_multi $ne re${RE_TAGS[i]} -var smode spatial -var dcad 10 \
+          -var emodel fixed_energy -var epar ${RE_EV[i]}
+    done
+  done
+fi
 echo "done -> state/redepo.*"
